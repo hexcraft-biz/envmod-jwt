bffa9ba35b995a9545e1af0ec35fa821c65a4214
Boss <cure1218@gmail.com>
2023-11-05 00:53:40 +0000

    boss

---
+++ README.md
@@ -0,0 +1,8 @@
+# env
+The blocks below are the env settings for the modules 
+
+## jwt
+```bash
+JWT_SIGNING_KEY=my_jwt_signing_key
+JWT_SIGNING_METHOD=HS512 # Values: HS256, HS384, HS512
+```
---
+++ core.go
@@ -0,0 +1,45 @@
+package jwt
+
+import (
+	"os"
+
+	"github.com/golang-jwt/jwt"
+)
+
+type Jwt struct {
+	SigningKey    []byte
+	SigningMethod *jwt.SigningMethodHMAC
+}
+
+func New() (*Jwt, error) {
+	var jwtSigningMethod *jwt.SigningMethodHMAC
+	switch os.Getenv("JWT_SIGNING_METHOD") {
+	case "HS256":
+		jwtSigningMethod = jwt.SigningMethodHS256
+	case "HS384":
+		jwtSigningMethod = jwt.SigningMethodHS384
+	default: // case "HS512":
+		jwtSigningMethod = jwt.SigningMethodHS512
+	}
+
+	return &Jwt{
+		SigningKey:    []byte(os.Getenv("JWT_SIGNING_KEY")),
+		SigningMethod: jwtSigningMethod,
+	}, nil
+}
+
+func (e Jwt) GenToken(claims jwt.Claims) (string, error) {
+	return jwt.NewWithClaims(e.SigningMethod, claims).SignedString(e.SigningKey)
+}
+
+func (e Jwt) ParseWithClaims(tokenStr string, claims jwt.Claims) (*jwt.Token, error) {
+	return jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return e.SigningKey, nil
+	})
+}
+
+func (e Jwt) Parse(tokenStr string) (*jwt.Token, error) {
+	return jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return e.SigningKey, nil
+	})
+}
